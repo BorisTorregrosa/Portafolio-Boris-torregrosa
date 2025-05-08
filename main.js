4c59f1909eeaf55ab076614f1682ffca77df2ace
BorisTorregrosa <torregrosaborisjose@gmail.com>
2025-05-08 00:02:11 -0500

    Portafolio BorisT modificado

--- main.js
+++ main.js
@@ -10,10 +10,12 @@ btn.addEventListener("click", function () {
     const Paleta = document.querySelector(".botonC");
     const navegacion = document.querySelector(".url");
     const Url = document.querySelectorAll(".letra");
+    const fondoMain = document.querySelector(".main"); // indicamos la clase donde esta el fondo del main portafolio
+    const LetraBienvenido = document.querySelector(".div1-main"); // letras del titulo de la presentacion del portafolio
+    const fondobody = document.querySelector("body"); 
     barra.style.backgroundColor = hexColor;
     Paleta.style.backgroundColor = hexColor;
     navegacion.style.backgroundColor = hexColor + "B3";
-
     function hexToRgb(hex) {
         const bigint = parseInt(hex.slice(1), 16);
         const r = (bigint >> 16) & 255;
@@ -21,21 +23,30 @@ btn.addEventListener("click", function () {
         const b = bigint & 255;
         return { r, g, b };
     }
-    
     function isDark(hexColor) {
         const { r, g, b } = hexToRgb(hexColor);
         const brightness = (r * 299 + g * 587 + b * 114) / 1000;
         return brightness < 128;
     }
     if(isDark(hexColor) ){
+
         Url.forEach(function(Url) {
-            Url.style.color = "#FFFFFF";
+            Url.style.color = "#FFFFFF"; // fondo de color blanco aqui
+
         });
+        fondobody.style.backgroundImage = 'url("imagenes/fondooscuro.png")';
+        fondoMain.style.backgroundImage = 'url("imagenes/fondooscuromain.png")';
+        LetraBienvenido.style.color = "#FFFFFF";
     }
     else{
         Url.forEach(function(Url) {
-            Url.style.color = "#000000";
+            Url.style.color = "#000000";  // fondo oscuro aqui
         });
+        fondobody.style.backgroundImage = 'url("imagenes/fondobody.jpg")';
+        fondoMain.style.backgroundImage = 'url("imagenes/main.jpg")';
+        LetraBienvenido.style.color = "#000000";
+        
+
     }
     
 });
